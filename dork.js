cd2b048d10362d76eebec9fca1ef75223ce378b0
lostfictions <lostfictions@users.noreply.github.com>
2018-01-31 04:24:35 -0500

    woooof

--- dork.js
+++ dork.js
@@ -1,15 +1,26 @@
-/* global emojiList */
+/* global emojiList, Snap */
 let [emojiName, codepoint] = randomInArray(Object.entries(emojiList))
 
 console.log(emojiName, codepoint)
 
-
 const o = document.createElement('object')
 o.type = 'image/svg+xml'
 o.data = `svg/emoji_u${codepoint}.svg`
-document.body.appendChild(o)
+o.style.width = '100%'
+o.style.height = '100%'
+document.querySelector('.container').appendChild(o)
 
-console.log(o.contentDocument.getElementsByClassName('g'))
-// console.dir(o.contentDocument.querySelectorAll('g'))
+o.addEventListener('load', () => {
+  const svg = o.contentDocument.querySelector('svg')
+  svg.setAttribute('viewBox', `0 0 128 128`)
+  fuckup()
+})
+
+function fuckup() {
+  Snap('object').selectAll('g').forEach(g => g.transform(`rotate(${Math.random() * 20 - 10}deg)`))
+  window.requestAnimationFrame(fuckup)
+}
 
 function randomInArray(arr) { return arr[Math.floor(Math.random() * arr.length)] }
+
+window.randomInArray = randomInArray
--- verify.js
+++ verify.js
@@ -7,3 +7,9 @@ const notFound = entries.filter(([, codepoints]) => !fs.existsSync(`svg/emoji_u$
 
 console.log(notFound.length)
 console.log(notFound.map(([name, cps]) => `${name} (${cps})`).join('\n'))
+
+const found = entries.filter(([, codepoints]) => fs.existsSync(`svg/emoji_u${codepoints}.svg`))
+const o = {}
+found.forEach(([n, cp]) => o[n] = cp)
+
+fs.writeFileSync('emojilist-gen.js', `const emojiList = ${JSON.stringify(o, undefined, 2)}\n\nif(typeof window != 'undefined') window.emojiList = emojiList\nelse if(module) module.exports = emojiList\n`)
