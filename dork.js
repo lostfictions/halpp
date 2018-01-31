6ad87798daae95eb56339aa0dc9874155fd0db97
lostfictions <lostfictions@users.noreply.github.com>
2018-01-31 04:34:03 -0500

    slight improvement

--- dork.js
+++ dork.js
@@ -10,17 +10,19 @@ o.style.width = '100%'
 o.style.height = '100%'
 document.querySelector('.container').appendChild(o)
 
+let groups
+
 o.addEventListener('load', () => {
   const svg = o.contentDocument.querySelector('svg')
   svg.setAttribute('viewBox', `0 0 128 128`)
+
+  groups = Snap('object').selectAll('g')
   fuckup()
 })
 
 function fuckup() {
-  Snap('object').selectAll('g').forEach(g => g.transform(`rotate(${Math.random() * 20 - 10}deg)`))
+  groups.forEach(g => g.transform(`rotate(${Math.random() * 20 - 10}deg)`))
   window.requestAnimationFrame(fuckup)
 }
 
 function randomInArray(arr) { return arr[Math.floor(Math.random() * arr.length)] }
-
-window.randomInArray = randomInArray
